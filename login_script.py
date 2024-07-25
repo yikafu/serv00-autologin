bbd7287b86d94c367bf9b90931d6647798b6669b
yikafu <f2149804524@gmail.com>
2024-07-25 20:44:38 +0800

    fix

--- login_script.py
+++ login_script.py
@@ -6,10 +6,11 @@ import os
 account = os.environ.get('ACCOUNT')
 accountList = json.loads(account)
 co = ChromiumOptions().headless()
-page = ChromiumPage(co)
 lst=[]
 
+
 def action(username, password, url):
+    page = ChromiumPage(co)
     page.get(url)
     page.wait(5)
     page.ele('#id_username').input(username)
@@ -20,6 +21,7 @@ def action(username, password, url):
     su_text = t.split(':')[1] + '登录成功'
     print(su_text)
     lst.append(su_text)
+    page.wait(10)
     page.close()
 
 
