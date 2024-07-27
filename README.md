9e519c6611fa70d16a3fd44b9af2b5eccbf5ef3d
yikafu <f2149804524@gmail.com>
2024-07-27 13:18:07 +0800

    add fuc

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # serv00-autologin
 
-自动化运行脚本实现 serv00 保活，默认每 30 天运行一次
+自动化运行脚本实现 serv00 保活，默认每 30 天运行一次，并通过企业微信机器人进行消息推送
 
 
 ## secrets 配置方式
 Settings -> Secrets and variables -> Repository secrets -> New Repository secrets
 
+### 账号密钥
 - Name 设置为 ACCOUNT            
 - Secret 格式如下
 
@@ -23,4 +24,10 @@ Settings -> Secrets and variables -> Repository secrets -> New Repository secret
     }
 ]
 
+```
+### 企业微信机器人
+- Name 设置为 WX_URL
+Secret 设置为 完整的url
+```
+https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=xxxx
 ```
--- login_script.py
+++ login_script.py
@@ -1,12 +1,14 @@
 import json
 from DrissionPage import ChromiumPage, ChromiumOptions
 import os
+import requests 
 
-# 从环境变量中获取 账号密码json
+# 从环境变量中获取 账号密码json、企业微信机器人webhook地址
 account = os.environ.get('ACCOUNT')
 accountList = json.loads(account)
+wx_url = os.environ.get('WX_URL')
 co = ChromiumOptions().headless()
-lst=[]
+message = ''
 
 
 def action(username, password, url):
@@ -18,17 +20,22 @@ def action(username, password, url):
     page.ele('#submit').click()
     page.wait(5)
     t = page.ele('css:#nav-menu-collapse > ul > li:nth-child(1) > p').inner_html
-    su_text = t.split(':')[1] + '登录成功'
+    su_text = t.split(':')[1] + '---登录成功'
     print(su_text)
-    lst.append(su_text)
+    message = message + su_text + '\n'
     page.wait(10)
     page.close()
 
+def send_markdown (message, wx_url):
+    data = {"msgtype": "markdown", "markdown": {"content": message}}
+    r = requests.post(url=wx_url,data=json.dumps(data))
+    print(r.text)
+    print(r.status_code)
 
 def main():
     for i in accountList:
         action(i['username'], i['password'], i['url'])
-    print(lst)
+    send_markdown(message)
 
 if __name__ == '__main__':
     main()
